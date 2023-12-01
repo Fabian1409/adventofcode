70c51fab857ff7f8545f5e36e2feb7d04cb95845
Fabian Gruber <fabian.gruber5@gmail.com>
2023-12-01 17:38:16 +0100

    .

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -1,7 +1,9 @@
-use std::{collections::BTreeMap, fs::read_to_string};
+#![allow(dead_code)]
+
+use std::collections::BTreeMap;
 
 fn part1(input: &str) -> usize {
-    read_to_string(input).unwrap().lines().fold(0, |acc, l| {
+    input.lines().fold(0, |acc, l| {
         let digits: Vec<_> = l.chars().filter(|c| c.is_ascii_digit()).collect();
         let first = *digits.first().unwrap() as usize - 0x30;
         let last = *digits.last().unwrap() as usize - 0x30;
@@ -14,7 +16,7 @@ fn part2(input: &str) -> usize {
     let digits_str = [
         "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
     ];
-    read_to_string(input).unwrap().lines().fold(0, |acc, l| {
+    input.lines().fold(0, |acc, l| {
         let mut digit_pos = BTreeMap::new();
         for (digit, digit_str) in digits_str.iter().enumerate() {
             if let Some(pos) = l.find(digit.to_string().as_str()) {
@@ -37,25 +39,32 @@ fn part2(input: &str) -> usize {
     })
 }
 
-fn main() {
-    let sum = part1("input_part1.txt");
-    println!("sum = {sum}");
-
-    let sum = part2("input_part2.txt");
-    println!("sum = {sum}");
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1("input_part1.txt"), 142)
+        let input = "
+            1abc2
+            pqr3stu8vwx
+            a1b2c3d4e5f
+            treb7uchet
+        ";
+        assert_eq!(part1(input.trim()), 142)
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2("input_part2.txt"), 281)
+        let input = "
+            two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen
+        ";
+        assert_eq!(part2(input.trim()), 281)
     }
 }
