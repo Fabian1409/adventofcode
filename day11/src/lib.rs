acf2eed425af070e8e8922ad3e70278894058201
Fabian Gruber <fabian.gruber@student.tugraz.at>
2023-12-11 13:58:28 +0100

    day11p2, and potential p1 speedup

--- day11/src/lib.rs
+++ day11/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use aoc_traits::AdventOfCodeDay;
 
 fn transpose(v: Vec<Vec<char>>) -> Vec<Vec<char>> {
@@ -27,28 +29,22 @@ impl<'a> AdventOfCodeDay<'a> for Day11Solver {
     type Part2Output = usize;
 
     fn solve_part1(input: &Self::ParsedInput) -> Self::Part1Output {
-        let mut expanded = Vec::new();
-        for row in input.iter() {
-            expanded.push(row.clone());
+        let mut y_expansions = HashMap::new();
+        for (i, row) in input.iter().enumerate() {
             if row.iter().all(|c| *c == '.') {
-                expanded.push(row.clone());
+                y_expansions.insert(i, 2);
             }
         }
 
-        let transposed = transpose(expanded);
-
-        let mut expanded = Vec::new();
-
-        for row in transposed.iter() {
-            expanded.push(row.clone());
+        let transposed = transpose(input.to_vec());
+        let mut x_expansions = HashMap::new();
+        for (i, row) in transposed.iter().enumerate() {
             if row.iter().all(|c| *c == '.') {
-                expanded.push(row.clone());
+                x_expansions.insert(i, 2);
             }
         }
 
-        let expanded = transpose(expanded);
-
-        let galaxies: Vec<_> = expanded
+        let galaxies: Vec<_> = input
             .iter()
             .enumerate()
             .flat_map(|(i, row)| {
@@ -66,14 +62,78 @@ impl<'a> AdventOfCodeDay<'a> for Day11Solver {
                 galaxies
                     .iter()
                     .skip(i + 1)
-                    .map(|other| distance(*galaxy, *other))
+                    .map(|other| {
+                        let x_adj_galaxy: usize = (0..galaxy.0)
+                            .map(|x| x_expansions.get(&x).unwrap_or(&1))
+                            .sum();
+                        let y_adj_galaxy: usize = (0..galaxy.1)
+                            .map(|y| y_expansions.get(&y).unwrap_or(&1))
+                            .sum();
+                        let x_adj_other: usize = (0..other.0)
+                            .map(|x| x_expansions.get(&x).unwrap_or(&1))
+                            .sum();
+                        let y_adj_other: usize = (0..other.1)
+                            .map(|y| y_expansions.get(&y).unwrap_or(&1))
+                            .sum();
+                        distance((x_adj_galaxy, y_adj_galaxy), (x_adj_other, y_adj_other))
+                    })
                     .collect::<Vec<_>>()
             })
             .sum()
     }
 
     fn solve_part2(input: &Self::ParsedInput) -> Self::Part2Output {
-        todo!()
+        let mut y_expansions = HashMap::new();
+        for (i, row) in input.iter().enumerate() {
+            if row.iter().all(|c| *c == '.') {
+                y_expansions.insert(i, 1_000_000);
+            }
+        }
+
+        let transposed = transpose(input.to_vec());
+        let mut x_expansions = HashMap::new();
+        for (i, row) in transposed.iter().enumerate() {
+            if row.iter().all(|c| *c == '.') {
+                x_expansions.insert(i, 1_000_000);
+            }
+        }
+
+        let galaxies: Vec<_> = input
+            .iter()
+            .enumerate()
+            .flat_map(|(i, row)| {
+                row.iter()
+                    .enumerate()
+                    .filter_map(|(j, c)| if *c == '#' { Some((j, i)) } else { None })
+                    .collect::<Vec<_>>()
+            })
+            .collect();
+
+        galaxies
+            .iter()
+            .enumerate()
+            .flat_map(|(i, galaxy)| {
+                galaxies
+                    .iter()
+                    .skip(i + 1)
+                    .map(|other| {
+                        let x_adj_galaxy: usize = (0..galaxy.0)
+                            .map(|x| x_expansions.get(&x).unwrap_or(&1))
+                            .sum();
+                        let y_adj_galaxy: usize = (0..galaxy.1)
+                            .map(|y| y_expansions.get(&y).unwrap_or(&1))
+                            .sum();
+                        let x_adj_other: usize = (0..other.0)
+                            .map(|x| x_expansions.get(&x).unwrap_or(&1))
+                            .sum();
+                        let y_adj_other: usize = (0..other.1)
+                            .map(|y| y_expansions.get(&y).unwrap_or(&1))
+                            .sum();
+                        distance((x_adj_galaxy, y_adj_galaxy), (x_adj_other, y_adj_other))
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .sum()
     }
 
     fn parse_input(input: &'a str) -> Self::ParsedInput {
@@ -121,7 +181,7 @@ mod test {
         ";
         assert_eq!(
             Day11Solver::solve_part2(&Day11Solver::parse_input(input.trim())),
-            8410
+            82_000_210
         );
     }
 }
