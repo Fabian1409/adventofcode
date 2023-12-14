9911f2d3f00515839fae2faab68e9835670e6237
Fabian Gruber <fabian.gruber5@gmail.com>
2023-12-14 17:52:45 +0100

    day14p2 and cleanup

--- day14/src/lib.rs
+++ day14/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use aoc_traits::AdventOfCodeDay;
 
 fn transpose(v: Vec<Vec<char>>) -> Vec<Vec<char>> {
@@ -13,6 +15,80 @@ fn transpose(v: Vec<Vec<char>>) -> Vec<Vec<char>> {
         .collect()
 }
 
+fn calc_load(input: Vec<Vec<char>>) -> usize {
+    input
+        .iter()
+        .map(|row| {
+            row.iter()
+                .enumerate()
+                .filter_map(|(i, c)| if *c == 'O' { Some(row.len() - i) } else { None })
+                .sum::<usize>()
+        })
+        .sum()
+}
+
+fn shift_left(input: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    input
+        .into_iter()
+        .map(|row| {
+            let mut round_nums = vec![(0, 0)];
+            for (i, c) in row.iter().enumerate() {
+                match c {
+                    'O' => round_nums.last_mut().unwrap().1 += 1,
+                    '#' => round_nums.push((i + 1, 0)),
+                    _ => {}
+                }
+            }
+            let n = row.len();
+            let mut row = vec!['.'; n];
+            for (pos, num) in round_nums {
+                if pos != 0 {
+                    row[pos - 1] = '#';
+                }
+
+                for c in row.iter_mut().skip(pos).take(num) {
+                    *c = 'O';
+                }
+            }
+            row
+        })
+        .collect()
+}
+
+fn shift_right(input: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    input
+        .into_iter()
+        .map(|row| {
+            let mut round = vec![(0, 0)];
+            for (i, c) in row.iter().rev().enumerate() {
+                match c {
+                    'O' => round.last_mut().unwrap().1 += 1,
+                    '#' => round.push((i + 1, 0)),
+                    _ => {}
+                }
+            }
+            let n = row.len();
+            let mut row = vec!['.'; n];
+            for (pos, num) in round {
+                if pos != 0 {
+                    row[n - pos] = '#';
+                }
+                for c in row.iter_mut().take(n - pos).skip(n - pos - num) {
+                    *c = 'O';
+                }
+            }
+            row
+        })
+        .collect()
+}
+
+fn cycle(input: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let north = shift_left(transpose(input));
+    let west = shift_left(transpose(north));
+    let south = shift_right(transpose(west));
+    shift_right(transpose(south))
+}
+
 pub struct Day14Solver;
 
 impl<'a> AdventOfCodeDay<'a> for Day14Solver {
@@ -23,31 +99,30 @@ impl<'a> AdventOfCodeDay<'a> for Day14Solver {
     type Part2Output = usize;
 
     fn solve_part1(input: &Self::ParsedInput) -> Self::Part1Output {
-        input
-            .iter()
-            .map(|row| {
-                let mut round = vec![(0, 0)];
-                for (i, c) in row.iter().enumerate() {
-                    match c {
-                        'O' => round.last_mut().unwrap().1 += 1,
-                        '#' => round.push((i + 1, 0)),
-                        _ => {}
-                    }
-                }
-                round
-                    .into_iter()
-                    .map(|(pos, num)| (pos..pos + num).map(|x| row.len() - x).sum::<usize>())
-                    .sum::<usize>()
-            })
-            .sum()
+        calc_load(shift_left(transpose(input.to_vec())))
     }
 
     fn solve_part2(input: &Self::ParsedInput) -> Self::Part2Output {
-        todo!()
+        let mut possible = HashMap::new();
+        let mut cur = input.clone();
+        for i in 0..1_000_000_000 {
+            cur = cycle(cur);
+            if let Some(j) = possible.insert(cur.clone(), i) {
+                let cycle_len = i - j;
+                let remaining = (1_000_000_000 - i) % cycle_len;
+
+                for _ in 0..remaining - 1 {
+                    cur = cycle(cur);
+                }
+
+                return calc_load(transpose(cur));
+            }
+        }
+        0
     }
 
     fn parse_input(input: &'a str) -> Self::ParsedInput {
-        transpose(input.lines().map(|l| l.trim().chars().collect()).collect())
+        input.lines().map(|l| l.trim().chars().collect()).collect()
     }
 }
 
@@ -91,7 +166,7 @@ mod test {
         ";
         assert_eq!(
             Day14Solver::solve_part2(&Day14Solver::parse_input(input.trim())),
-            0
+            64
         );
     }
 }
